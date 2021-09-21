5b95c71a0d576fa5eadcb430404672dc8280d307
Jose Rios <joserios055@gmail.com>
2021-09-21 17:56:31 -0500

    Adding solution proposed exercises

---
+++ ejercicios_propuestos/parte1_ejer20.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <math.h>
+
+int factorial(int numero);
+float suma(int x, int terminos);
+
+int main() {
+
+    int x;
+    int terminos;
+    
+    printf("\n Ingrese el valor de entrada de x: ");
+    scanf("%d", &x);
+	
+    printf("\n Ingrese el numero de terminos: "); 
+    scanf("%d", &terminos);
+	
+    printf("\n El resultado de la suma de las series es: %f \n", suma(x, terminos));
+	
+}
+
+int factorial(int numero){
+    int resultado = 1;
+    for(int i = 1; i <= numero; i++){
+        resultado = resultado*i;
+    }
+    return resultado;
+}
+
+float suma(int x, int terminos) {
+
+        int signo = 1, y = 2;
+        double formula;
+    	float suma = 1;
+
+	for (int i = 1; i < terminos; i++) {
+           signo = -1 * signo;
+           formula = signo * (pow(x, y)) / factorial(y);
+	   suma = suma + formula;
+           y = y + 2;
+	}
+	return suma;
+}
+

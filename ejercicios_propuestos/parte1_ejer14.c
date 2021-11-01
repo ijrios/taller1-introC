a4346fd5a23034b7e1f642f0f0829ff0694235e5
Jose Alexander Ríos Trespalacios <joserios055@gmail.com>
2021-11-01 05:44:34 -0500

    Adding solution of proposed exercises

---
+++ ejercicios_propuestos/parte1_ejer14.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include<stdbool.h>
+
+int multiplos(int x, int y);
+int NumeroAleatorio();
+
+int main(){
+    
+  int x, n, d; 
+  srand(time(NULL));
+  x = NumeroAleatorio();
+  printf("El numero elegido para el PUM es %d \n", x);
+  printf("Ingrese el numero de jugadores: ");
+  scanf("%d",&n);
+  
+
+    for (int j = 1; j <= 500; j++)  
+     for (int i = 1; i <= n; i++) {
+        d = multiplos(x,j); 
+      if(d = 2){      
+      printf ("%d    %d \n ",j,i);
+      j = j + 1;
+      } else if(d = 2){
+      printf ("%c    %d \n ","PUM",i);
+      j = j + 1;
+      }
+     
+  }
+}
+
+int multiplos(int n1, int n2){
+
+  int  mayor, menor, r = 0;
+  mayor = n1;
+  menor = n2;
+    
+     if ( n2 > n1 )
+          {        
+         mayor = n2;
+         menor = n1;
+          }        
+
+     if (0 == mayor % menor){
+      //El numero es multiplo del numero 
+      r = 1;
+     }
+      else if (0 != mayor % menor){
+      //Los numeros no son multiplos.
+      r = 2;
+      }
+     return r;
+}
+
+int NumeroAleatorio()
+{
+    int x;
+    x = rand() % 10;
+    return x;
+}
+
